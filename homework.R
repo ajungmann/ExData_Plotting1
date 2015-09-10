67fad27c6b0a869880f8f36662ab9c8651c375ce
ajungmann <alfonsojungmann@hotmail.com>
2015-09-10 16:26:26 -0300

    New Code

--- homework.R
+++
@@ -1,6 +0,0 @@
-dataFile<-"household_power_consumption.txt"
-data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
-data2 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-data2$Global_active_power<-as.numeric(data2$Global_active_power)
-hist(data2$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
-
