3fc21a6a6ce55efd0e8b07e5668e555e7b6f75dd
ALFONSOJUNGMANN <alfonsojungmann@hotmail.com>
2015-09-10 17:41:38 -0300

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,8 @@
 dataFile<-"household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+###Subset###
 data2 <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 data2$Global_active_power<-as.numeric(data2$Global_active_power)
+##Graphic###
 hist(data2$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
 
